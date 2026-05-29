aeb20b2df950dde7fbc4a82de8ab7b3d25739c08
meghanadhjanga-245 <meghanadhjanga02@gmail.com>
2026-05-29 09:22:06 +0530

    Time: 1 ms (81.04%), Space: 40.9 MB (99.99%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,22 @@
 class Solution {
-    public boolean isHappy(int n){ 
-    int sum=0;
-    if(n==1){
+    public boolean isHappy(int n) {
+      HashSet<Integer> megha=new HashSet<>();
+      if(n==1){
         return true;
-    }
-    if(n==4){
-        return false;
-    }
-        while(n!=1 && n!=4){
-            sum=0;
+      }  
+      while(n!=1){
+        if(megha.contains(n)){
+            return false;
+        }
+        megha.add(n);
+        int sum=0;
         while(n>0){
             int digit=n%10;
-            sum=sum+(digit*digit);
+            sum+=digit*digit;
             n=n/10;
         }
         n=sum;
-        }
-        return sum==1;
+      }
+      return true;
     }
 }
