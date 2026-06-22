f665ae2191a1159797327e9f30b5866804a3f3fe
meghanadhjanga-245 <meghanadhjanga02@gmail.com>
2026-06-22 22:02:54 +0530

    Time: 6 ms (44.11%), Space: 44.6 MB (60.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int m=s.length();
-        int n=t.length();
-        if(m!=n){
+        if(s.length()!=t.length()){
             return false;
         }
-        else{
-            int count[]=new int[26];
-            for(int i=0;i<m;i++){
-                count[s.charAt(i)-'a']++;
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int count:freq){
+            if(count!=0){
+                return false;
             }
-            for(int i=0;i<n;i++){
-                count[t.charAt(i)-'a']--;
-            }
-            for(int i=0;i<count.length;i++){
-                if(count[i]!=0){
-                    return false;
-                }
-            }
-        } 
+        }
         return true;
     }
 }
